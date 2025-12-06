a3c722d90c0eb1a8e2e847e8ad382f0ef78d08bb
Azwin-1997 <aswin.panangatt@gmail.com>
2025-12-06 16:21:58 +0530

    changed port

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
-import CartProvider from './provider/cartProvider.jsx'
-import SearchProvider from './provider/SearchProvider.jsx'
+import CartProvider from './context/CartProvider.jsx'
+import SearchProvider from './context/SearchProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
