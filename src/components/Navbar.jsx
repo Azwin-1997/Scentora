a61abe89c61f27a35a334a22d6d7fb268eb4e3be
Azwin-1997 <aswin.panangatt@gmail.com>
2025-12-06 16:26:48 +0530

    updated path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { CartContext } from "../context/CartContext";
-import { SearchContext } from "../context/SearchContext";
+import { CartContext } from "../context/CartContext.jsx";
+import { SearchContext } from "../context/SearchContext.jsx";
 
 function Navbar() {
   const navigate = useNavigate();
