37bc49eead2664e851083c583ad773407a77ef9c
Azwin-1997 <aswin.panangatt@gmail.com>
2025-12-06 16:31:04 +0530

    updated path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { CartContext } from "../context/CartProvider";
-import { SearchContext } from "../context/SearchProvider";
+import { CartContext } from "../context/CartContext";
+import { SearchContext } from "../context/SearchContext";
 
 function Navbar() {
   const navigate = useNavigate();
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
-import CartProvider from './context/CartProvider.jsx'
-import SearchProvider from './context/SearchProvider.jsx'
+import CartProvider from './context/CartContext.jsx'
+import SearchProvider from './context/SearchContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
